fbb7a1a19de750d594905fd065eee018f8bace64
SethZarko <sethzarkovich@gmail.com>
2024-04-12 20:10:30 -0400

    Test Options API

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,28 @@
 <script lang="ts">
-
+  export default {
+    data() {
+      return {
+        helloWorld: ''
+      }
+    },
+    watch: {
+      helloWorld(newState, oldState) {
+        if(newState) {
+          console.log('State Changed');
+        }
+      }
+    },
+    methods: {
+      hello() {
+        this.helloWorld = 'Hello, World'
+      }
+    },
+    mounted(){
+      this.hello()
+    }
+  }
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello, World!</h1>
+  <h1 class="text-3xl font-bold underline">{{ helloWorld }}</h1>
 </template>
